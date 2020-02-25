93b6995e6099ffd5e2f8bf5533418b6d8c95b068
github username <github email address>
2020-02-24 21:04:48 -0700

    front page styling

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("HI!")
+
+const hamburgerLink = document.querySelector('.hamburger')
+hamburgerLink.addEventListener("click", dropDown) 
+
+function dropDown(){
+    console.log("Hello there")
+}
