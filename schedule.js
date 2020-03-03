c32d26d721e43ce3afd14290b0418d18317e6610
github username <github email address>
2020-03-03 10:12:47 -0700

    added more styling to scheduling, brought in location information, and created a show page for the location related to a specific game

--- schedule.js
+++ schedule.js
@@ -1,33 +1,44 @@
-
 const tbody = document.querySelector('tbody')
 const trTag = document.querySelector('tr')
 const main = document.querySelector('main')
 const scheduleHeader= document.querySelector('#team-picture-container')
 
+
 fetch('http://localhost:3000/team_games')
 .then(response => response.json())
 .then(teamGames=> {
-    teamGames.map(game=> {
-        console.log(game)
+    console.log(teamGames)
+    teamGames.map(gameObject=> {
+        // console.log(game)
         let gameContainer = document.createElement('div')
         gameContainer.className = 'game-div-container'
 
-
-        let rivalName = document.createElement('h5')
+        let rivalName = document.createElement('h4')
         let date = document.createElement('h5')
         let time = document.createElement('h5')
-        let location = document.createElement('h5')
         let score = document.createElement('h5')
-
-        rivalName.innerText = game.rival.name
-        date.innerText = game.game.date
-        time.innerText = game.game.time
-        location.innerText = game.game.location_id
-        score.innerText = game.game.score_id
+        let teamImage = document.createElement('img')
+        let locationId = document.createElement('h5')
+        let locationName = document.createElement('h5')
         
+
+        
+        rivalName.innerText = gameObject.rival.name
+        date.innerText = gameObject.game.date
+        time.innerText = gameObject.game.time
+        score.innerText = gameObject.game.score_id
+        teamImage.src = gameObject.rival.image
+        teamImage.className = "team-logo"
+        locationName.innerHTML = `<a href="about_game_location.html?id=${gameObject.game.location_id}">${gameObject.game.location.name}</a>`
        
+
+            
         main.append(gameContainer)
-        gameContainer.append(rivalName, date, time, location, score)
+        gameContainer.append(teamImage,rivalName, date, time, score, locationName)
+
+        // gameContainer.addEventListener('click', event => {
+            
+        // })
     })
 
     let headerTitle = document.createElement('h1')
@@ -37,15 +48,11 @@ fetch('http://localhost:3000/team_games')
     headerTitle.className = 'header-title'
     headerTitle.innerText = "Schedule"
     scheduleHeader.append(headerTitle, headerImg)
-
 })
 
-// fetch('http://localhost:3000/games')
-// .then(response => response.json())
-// .then(games => {
-//     games.map(game => {
-//         // let tdTagLocation= document.createElement('td')
-//         // tdTagLocation.innerText = game.location.name
-//         // trTag.append(tdTagLocation)
-//     })
-// })
+
+
+
+
+
+
